a831d1843a01ecb5c434b039ab4bcb24351a0c3e
Bryson Matsuda <han3wf@virginia.edu>
2023-10-16 02:01:00 -0400

    close to finishing randomNumGen method

--- MonteCarlo.java
+++ MonteCarlo.java
@@ -1,6 +1,7 @@
 import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class MonteCarlo{
     private final int MULTIPLIER = 24693;
@@ -9,14 +10,14 @@ public class MonteCarlo{
     public MonteCarlo(){
 
     }
-    public int randomNumberGenerator(int seed, int trials){
+    public long randomNumberGenerator(long seed, int trials){
         double totalTime = 0;
         if(trials < 0){
             return 0;
         }else {
             int numCalls = 0; //starts at 0;
             double uValue = 0;
-            int xValue = 0;
+            long xValue = 0;
             while (numCalls < 3) {
                 xValue = ((MULTIPLIER * seed + INCREMENT) % MODULUS);
                 uValue = (double) xValue / MODULUS;
@@ -28,7 +29,7 @@ public class MonteCarlo{
                 }
                 numCalls++;
             }
-            if(numCalls != 3) {
+            if(numCalls < 3) {
                 totalTime += numCalls * 3; //num * 3 is the time of first call given number of times through switchboard
                 totalTime += 2 * (numCalls - 1); //hangup time for failed calls
                 totalTime += 5; //+5 is for time after success to get into contact with representative
@@ -36,7 +37,11 @@ public class MonteCarlo{
                 totalTime += representativeTime; //total time on phone with representative
                 DecimalFormat df = new DecimalFormat("#.###");
                 System.out.println("Total Time: " + df.format(totalTime));
-                System.out.println("Representative Time" + representativeTime);
+            }else{
+                totalTime += 9; //3 first calls
+                totalTime += 4; //2 failed calls
+                DecimalFormat df = new DecimalFormat("#.###");
+                System.out.println("Total Time: " + df.format(totalTime));
             }
 
 
@@ -53,19 +58,17 @@ public class MonteCarlo{
     public int assignRepresentativeTotalTime(){
         String representative = "";
         int randomNumber = (int) (Math.random() * 10);
-        for(int i = 1; i < 11; i++){
-            if(randomNumber == 1 || randomNumber == 2){
-                representative = "A";
-            }
-            if(randomNumber == 3 || randomNumber == 4 || randomNumber == 5){
-                representative = "B";
-            }
-            if(randomNumber == 6){
-                representative = "C";
-            }
-            if(randomNumber == 7 || randomNumber == 8 || randomNumber == 9 || randomNumber == 10){
-                representative = "D";
-            }
+        if(randomNumber == 1 || randomNumber == 2){
+            representative = "A";
+        }
+        if(randomNumber == 3 || randomNumber == 4 || randomNumber == 5){
+            representative = "B";
+        }
+        if(randomNumber == 6){
+            representative = "C";
+        }
+        if(randomNumber == 7 || randomNumber == 8 || randomNumber == 9 || randomNumber == 10){
+            representative = "D";
         }
         if(representative.equals("A")){
             return 72;
